42e94e08443a1d4a29bd1e457eda625c9883c606
facundo cordoba <facucor.15@gmail.com>
2020-11-01 23:33:06 -0300

    P.S-02: paragraphs and index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+function isStr(str) {
+   return typeof str === "string"
+}
+
+function isNum(number) {
+    return typeof number === "number"
+}
+
+function validateName(name) {
+    if (!isStr(name)) {
+        return false;
+    }
+    name = name.trim();
+    if (name.length < 6) {
+        return false;
+    }
+    if (name.split(" ").length === 1) {
+        return false;
+    }
+    return true;
+}
+
+function validateEmail(email) {
+    if (!isStr(email)) {
+        return false;
+    }
+    email = email.trim();
+    var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    return !(email.match(emailRegex) == null);
+}
+
+function validatePass(pass) {
+    
+    var passRegex = /^[\w]{8,14}}$/g;
+    return !(pass.match(passRegex) == null);
+}
+
+function validateAge(age) {
+    if (typeof age !== "number") {
+        return false;
+    }
+    if (age < 18) {
+        return false;
+    }
+    return true;
+}
+
+function validatePhone(phone) {
+    if (!isNum(phone)) {
+        return false;
+    }
+    if (phone.length < 7) {
+        return false;
+    }
+}
+
+function validateAddress(address) {
+    if (!isStr(address)) {
+        return false;
+    }
+    if (address.length < 8) {
+        return false;
+    }
+    var addressRegex = /^[\w-\.]+ ([\w-]+)$/g;
+    return !(address.match(addressRegex) == null);
+}
